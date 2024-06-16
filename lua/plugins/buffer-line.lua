e2c5187ce9b6125eb1237fe5333e85e555cc8886
Davide Dal Santo <d.dalsanto@almaviva.it>
2024-06-16 20:03:10 +0200

    Added gitingore + bufferline
    Bufferline plugin comodo per gestire i vari buffer aperti. Aggiunto
    anche dei keybindings per ciclare tra i buffer aperti

---
+++ lua/plugins/buffer-line.lua
@@ -0,0 +1,25 @@
+return {
+  "akinsho/bufferline.nvim",
+  version = "*",
+  dependencies = "nvim-tree/nvim-web-devicons",
+  config = function()
+    -- require("bufferline").setup()
+    vim.opt.termguicolors = true
+    require("bufferline").setup({
+      options = {
+        diagnostics = "nvim_lsp",
+        diagnostics_indicator = function(count, level, diagnostics_dict, context)
+          local s = " "
+          for e, n in pairs(diagnostics_dict) do
+            local sym = e == "error" and " " or (e == "warning" and " " or "")
+            s = s .. n .. sym
+          end
+          return s
+        end,
+        -- cicla tra i buffer aperti
+        vim.keymap.set("n", "<leader>bn", ":bn<CR>", {}),
+        vim.keymap.set("n", "<leader>bp", ":bp<CR>", {}),
+      },
+    })
+  end,
+}
