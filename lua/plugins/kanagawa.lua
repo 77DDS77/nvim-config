a5747ef1b8f2d48a0ab2053e2b7b1af62aa392f1
Davide Dal Santo <d.dalsanto@almaviva.it>
2024-06-16 12:39:44 +0200

    first commit

---
+++ lua/plugins/kanagawa.lua
@@ -0,0 +1,7 @@
+return {
+  "rebelot/kanagawa.nvim",
+  config = function()
+    vim.cmd.colorscheme("kanagawa-wave")
+   -- vim.cmd.colorscheme("kanagawa-lotus") --light-theme
+  end,
+}
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,13 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+--			autoinstall = true,
+      ensure_installed = { "javascript", "c", "lua", "vim", "vimdoc", "query", "angular" },
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
+}
