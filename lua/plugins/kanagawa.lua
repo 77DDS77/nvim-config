f5f55c8fb1cf9ea86c3e5564d44658d5a125229b
Davide Dal Santo <d.dalsanto@almaviva.it>
2024-06-17 17:22:52 +0200

    updated rows number colors

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -2,6 +2,9 @@ return {
   "rebelot/kanagawa.nvim",
   config = function()
     vim.cmd.colorscheme("kanagawa-wave")
-   -- vim.cmd.colorscheme("kanagawa-lotus") --light-theme
+    -- vim.cmd.colorscheme("kanagawa-lotus") --light-theme
+    vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#51B3EC", bold = true })
+    vim.api.nvim_set_hl(0, "LineNr", { fg = "white", bold = true })
+    vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#FB508F", bold = true })
   end,
 }
