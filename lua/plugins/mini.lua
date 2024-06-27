be6a10fd178f299f66a48d221ab7f4648c4d127e
Davide Dal Santo <d.dalsanto@almaviva.it>
2024-06-27 11:02:57 +0200

    mini indent line - themed for onedark theme

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,10 +1,19 @@
 return {
 	{
-    --mini move, per muovere blocchi di codice con Alt + hjkl
+		-- mini move, per muovere blocchi di codice con Alt + hjkl
 		"echasnovski/mini.move",
 		version = "*",
 		config = function()
 			require("mini.move").setup()
+		end,
+	},
+	{
+		-- mini indent line
+		"nvimdev/indentmini.nvim",
+		config = function()
+			require("indentmini").setup() -- use default config
+			vim.cmd.highlight("IndentLine guifg=#363c51")
+			vim.cmd.highlight("IndentLineCurrent guifg=#25747d")
 		end,
 	},
 }
