b235e5cd434b46aa92287feb21bfdec6c30466e3
Davide Dal Santo <d.dalsanto@almaviva.it>
2024-07-02 10:31:53 +0200

    saving configuration pre overhaul

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,3 +6,26 @@ vim.cmd("set number")
 vim.cmd("set relativenumber")
 vim.g.mapleader = " "
 vim.api.nvim_set_option("clipboard", "unnamed")
+
+-- diagnostic updates while writing
+vim.diagnostic.config({
+    update_in_insert = true,
+})
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS the search contains capitals letters
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Show which line your cursor is on (da provare)
+vim.opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
+
+-- disable word wrap
+vim.opt.wrap = false
+
+vim.opt.termguicolors = true
