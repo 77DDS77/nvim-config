d114ea54a3ec264523386a0fdcaa7621d340b5cb
Davide Dal Santo <d.dalsanto@almaviva.it>
2024-07-02 16:46:39 +0200

    new config test

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,92 +1,85 @@
 return {
-	{
-		"hrsh7th/cmp-nvim-lsp",
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
+	dependencies = {
+		"hrsh7th/cmp-buffer", -- source for text in buffer
+		"hrsh7th/cmp-path", -- source for file system paths
+		"L3MON4D3/LuaSnip", -- snippet engine
+		"saadparwaiz1/cmp_luasnip", -- for autocompletion
+		"rafamadriz/friendly-snippets", -- useful snippets
+		"onsails/lspkind.nvim", -- vs-code like pictograms
 	},
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
-		},
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		config = function()
-			local cmp = require("cmp")
-			require("luasnip.loaders.from_snipmate").lazy_load()
+	enabled = true,
+	config = function()
+		local cmp = require("cmp")
 
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
+		local luasnip = require("luasnip")
+
+		local lspkind = require("lspkind")
+
+		local has_words_before = function()
+			unpack = unpack or table.unpack
+			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+		end
+
+		-- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
+		require("luasnip.loaders.from_vscode").lazy_load()
+		cmp.setup({
+			completion = {
+				completeopt = "menu,menuone,preview,noselect",
+			},
+			snippet = { -- configure how nvim-cmp interacts with snippet engine
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+				["<C-e>"] = cmp.mapping.abort(), -- close completion window
+				["<CR>"] = cmp.mapping.confirm({ select = false }),
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+						-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+						-- that way you will only jump inside the snippet region
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					elseif has_words_before() then
+						cmp.complete()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+			}),
+			-- sources for autocompletion
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" }, -- snippets
+				{ name = "buffer" }, -- text within current buffer
+				{ name = "path" }, -- file system paths
+			}),
+			-- configure lspkind for vs-code like pictograms in completion menu
+			formatting = {
+				format = lspkind.cmp_format({
+					maxwidth = 50,
+					ellipsis_char = "...",
 				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" }, -- For luasnip users.
-				}, {
-					{ name = "buffer" },
-				}),
-			})
-		end,
-	},
-	-- {
-	--   "hrsh7th/nvim-cmp",
-	--   dependencies = {
-	--     "luckasRanarison/tailwind-tools.nvim",
-	--     "onsails/lspkind-nvim",
-	--     "L3MON4D3/LuaSnip", -- Assicurati di avere LuaSnip come dipendenza
-	--     "saadparwaiz1/cmp_luasnip", -- Assicurati di avere cmp_luasnip come dipendenza
-	--   },
-	--   config = function()
-	--     local cmp = require("cmp")
-	--     local lspkind = require("lspkind")
-	--     local tailwind_tools = require("tailwind-tools.cmp")
-	--
-	--     -- Carica gli snippet da snipmate
-	--     require("luasnip.loaders.from_snipmate").lazy_load()
-	--
-	--     -- Configura nvim-cmp
-	--     cmp.setup({
-	--       snippet = {
-	--         expand = function(args)
-	--           require("luasnip").lsp_expand(args.body)
-	--         end,
-	--       },
-	--       window = {
-	--         completion = cmp.config.window.bordered(),
-	--         documentation = cmp.config.window.bordered(),
-	--       },
-	--       mapping = cmp.mapping.preset.insert({
-	--         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-	--         ["<C-f>"] = cmp.mapping.scroll_docs(4),
-	--         ["<C-Space>"] = cmp.mapping.complete(),
-	--         ["<C-e>"] = cmp.mapping.abort(),
-	--         ["<CR>"] = cmp.mapping.confirm({ select = true }),
-	--       }),
-	--       sources = cmp.config.sources({
-	--         { name = "nvim_lsp" },
-	--         { name = "luasnip" }, -- For luasnip users.
-	--       }, {
-	--         { name = "buffer" },
-	--       }),
-	--       formatting = {
-	--         format = lspkind.cmp_format({
-	--           before = tailwind_tools.lspkind_format
-	--         }),
-	--       },
-	--     })
-	--   end,
-	-- }
+			},
+		})
+	end,
 }
